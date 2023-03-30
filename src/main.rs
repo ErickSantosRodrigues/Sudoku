94238f5eb93a5a4587bb9d569fc8daf48aac4d72
ErickSantosRodrigues <esrerickssantos@gmail.com>
2023-03-29 23:38:33 -0300

    innitial commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use rand::Rng;
+// sudoku game 
+// for each line, collunm and group of nine is need to generate some non repetitive number
+// range from 1 to 9
+// struct map {
+//     vec<i32>
+// }
+
+fn print_game(map: Vec<Vec<i32>>) {
+    
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut map: Vec<Vec<i32>> = Vec::new();
+    let mut rng = rand::thread_rng();
+    for _a in 0..9{
+        let mut new: Vec<i32> = Vec::new();
+        for _b in 0..9{
+            new.push(rng.gen_range(1..=9));
+        }
+        map.push(new);
+    }
+
+    println!("{:?}", map);
+
 }
